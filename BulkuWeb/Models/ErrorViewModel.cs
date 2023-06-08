2a8c7d9e5e40be0e92b0db13f31582d971505e53
Abhi077777 <120783717+Abhi077777@users.noreply.github.com>
2023-06-08 10:12:44 +0530

    Add project files.

---
+++ BulkuWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkuWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
